218a3a5d725cd22154700ece874c082336feb057
wxd-gaming <492794628@qq.com>
2024-08-08 09:27:09 +0800

    【新增】初始化目录结构

---
+++ data-excel/src/main/java/wxdgaming/spring/boot/data/excel/DataExcelScan.java
@@ -0,0 +1,13 @@
+package wxdgaming.spring.boot.data.excel;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 扫描器
+ *
+ * @author: wxd-gaming(無心道, 15388152619)
+ * @version: 2024-08-08 09:25
+ **/
+@ComponentScan
+public class DataExcelScan {
+}
