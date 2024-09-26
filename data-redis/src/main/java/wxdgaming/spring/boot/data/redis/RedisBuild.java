fa01cdc3fe5681879cccb0faa964e19a4f8a673c
wxd-gaming <492794628@qq.com>
2024-09-26 09:08:33 +0800

    【优化】redis多数据源

--- data-redis/src/main/java/wxdgaming/spring/boot/data/redis/RedisBuild.java
+++ data-redis/src/main/java/wxdgaming/spring/boot/data/redis/RedisBuild.java
@@ -3,16 +3,20 @@ package wxdgaming.spring.boot.data.redis;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
@@ -32,30 +36,23 @@ import wxdgaming.spring.boot.core.InitPrint;
 @ConfigurationProperties(prefix = "spring.redis")
 public class RedisBuild implements CachingConfigurer, InitPrint {
 
-    @Getter
-    @Setter
-    public static class Builder {
-        private String host;
-        private int port;
-        private String password;
-        /** redis index db0 db1 */
-        private int database = 0;
-    }
 
-    private Builder first;
-    private Builder second;
+    private RedisProperties first;
+    private RedisProperties second;
 
+    @Primary
     @Bean("redisConnectionFactory")
     @ConditionalOnProperty("spring.redis.first.host")
     public RedisConnectionFactory redisConnectionFactory() {
         return redisConnectionFactory(first);
     }
 
-    @Bean("redisTemplate")
     @Primary
+    @Bean("redisTemplate")
     @ConditionalOnProperty("spring.redis.first.host")
-    public RedisTemplate<?, ?> redisTemplate(@Qualifier("redisConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
-        return build(redisConnectionFactory);
+    public RedisTemplate<?, ?> redisTemplate(
+            @Qualifier("redisConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
+        return buildRedisTemplate(redisConnectionFactory);
     }
 
     @Bean("secondRedisConnectionFactory")
@@ -66,20 +63,41 @@ public class RedisBuild implements CachingConfigurer, InitPrint {
 
     @Bean("secondRedisTemplate")
     @ConditionalOnProperty("spring.redis.second.host")
-    public RedisTemplate<?, ?> secondRedisTemplate(@Qualifier("secondRedisConnectionFactory") RedisConnectionFactory secondRedisConnectionFactory) {
-        return build(secondRedisConnectionFactory);
+    public RedisTemplate<?, ?> secondRedisTemplate(
+            @Qualifier("secondRedisConnectionFactory") RedisConnectionFactory secondRedisConnectionFactory) {
+        return buildRedisTemplate(secondRedisConnectionFactory);
     }
 
-    RedisConnectionFactory redisConnectionFactory(Builder builder) {
-        RedisStandaloneConfiguration standaloneConfiguration = new RedisStandaloneConfiguration();
-        standaloneConfiguration.setHostName(builder.getHost());
-        standaloneConfiguration.setPort(builder.getPort());
-        standaloneConfiguration.setPassword(builder.getPassword());
-        standaloneConfiguration.setDatabase(builder.getDatabase());
-        return new JedisConnectionFactory(standaloneConfiguration);
+    RedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisProperties.getHost());
+        redisStandaloneConfiguration.setPort(redisProperties.getPort());
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(redisProperties.getPassword()));
+        redisStandaloneConfiguration.setDatabase(redisProperties.getDatabase());
+        redisStandaloneConfiguration.setUsername(redisProperties.getUsername());
+
+        GenericObjectPoolConfig<?> poolConfig = buildGenericObjectPoolConfig(redisProperties);
+
+        LettucePoolingClientConfiguration.LettucePoolingClientConfigurationBuilder builder = LettucePoolingClientConfiguration.builder();
+        builder.poolConfig(poolConfig);
+        if (redisProperties.getTimeout() != null)
+            builder.commandTimeout(redisProperties.getTimeout());
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, builder.build());
     }
 
-    RedisTemplate<?, ?> build(RedisConnectionFactory redisConnectionFactory) {
+    private static GenericObjectPoolConfig<?> buildGenericObjectPoolConfig(RedisProperties redisProperties) {
+        GenericObjectPoolConfig<?> poolConfig = new GenericObjectPoolConfig<>();
+        poolConfig.setMaxTotal(redisProperties.getLettuce().getPool().getMaxActive());
+        poolConfig.setMaxIdle(redisProperties.getLettuce().getPool().getMaxIdle());
+        poolConfig.setMinIdle(redisProperties.getLettuce().getPool().getMinIdle());
+        if (redisProperties.getLettuce().getPool().getMaxWait() != null) {
+            poolConfig.setMaxWait(redisProperties.getLettuce().getPool().getMaxWait());
+        }
+        return poolConfig;
+    }
+
+    RedisTemplate<?, ?> buildRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
         // 默认的序列化器： new JdkSerializationRedisSerializer()
         redisTemplate.setKeySerializer(new StringRedisSerializer());
