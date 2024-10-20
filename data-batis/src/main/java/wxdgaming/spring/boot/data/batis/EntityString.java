91da13b19dcfbf80648e7ae5e50c254fa2b9dcc5
wxdgaming <492794628@qq.com>
2024-10-20 11:43:24 +0800

    【优化】异常过滤

--- data-batis/src/main/java/wxdgaming/spring/boot/data/batis/EntityString.java
+++ data-batis/src/main/java/wxdgaming/spring/boot/data/batis/EntityString.java
@@ -1,14 +1,14 @@
 package wxdgaming.spring.boot.data.batis;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import wxdgaming.spring.boot.core.lang.ObjectBase;
 
 /**
- * 自增id
- *
  * @author: wxd-gaming(無心道, 15388152619)
  * @version: 2024-09-16 23:00
  **/
@@ -16,11 +16,10 @@ import wxdgaming.spring.boot.core.lang.ObjectBase;
 @Setter
 @Accessors(chain = true)
 @MappedSuperclass
-public class EntityAutoBase<ID> extends ObjectBase {
+public class EntityString<ID> extends ObjectBase {
 
     @Id
     @Column
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private ID uid;
     @Column
     private String createdTime;
