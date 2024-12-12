eadac1f64e3ff86d13b5eb6daf4379879a20d2dc
wxd-gaming <492794628@qq.com>
2024-12-12 08:53:36 +0800

    【測試】h2數據庫不支持複雜的字段類型

--- data/src/main/java/wxdgaming/spring/boot/data/MyTestEntity.java
+++ data/src/main/java/wxdgaming/spring/boot/data/MyTestEntity.java
@@ -1,10 +1,12 @@
 package wxdgaming.spring.boot.data;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Comment;
 
 /**
  * 测试用的实体类
@@ -18,7 +20,13 @@ import lombok.Setter;
 @Table(indexes = {@Index(columnList = "name")})
 public class MyTestEntity extends EntityBase<Long> {
 
+    @Column(columnDefinition = "varchar(128)", nullable = false)
+    @Comment("姓名")
     private String name;
+    @Column(columnDefinition = "varchar(128)")
+    private String name2;
+    @Column(columnDefinition = "varchar(128)")
+    private String name3;
 
     public MyTestEntity() {
         setUid(System.nanoTime());
