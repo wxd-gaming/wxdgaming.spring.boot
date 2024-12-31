33df45ada562d0b8a5ca761b23781e7eac74ea8b
wxd-gaming <492794628@qq.com>
2024-12-31 09:18:42 +0800

    【新增】lua测试代码

---
+++ script-lua/src/test/java/code/LuaSpringReflect.java
@@ -0,0 +1,13 @@
+package code;
+
+import org.springframework.stereotype.Component;
+import wxdgaming.spring.boot.core.SpringReflect;
+
+/**
+ * @author: wxd-gaming(無心道, 15388152619)
+ * @version: 2024-12-30 20:47
+ **/
+@Component
+public class LuaSpringReflect extends SpringReflect {
+
+}
--- script-lua/src/test/java/code/LuaTest.java
+++ script-lua/src/test/java/code/LuaTest.java
@@ -1,23 +1,21 @@
 package code;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
-import party.iroiro.luajava.Lua;
 import wxdgaming.spring.boot.core.CoreScan;
-import wxdgaming.spring.boot.core.SpringUtil;
-import wxdgaming.spring.boot.core.io.FileUtil;
 import wxdgaming.spring.boot.data.redis.DataRedisScan;
-import wxdgaming.spring.boot.lua.LuaFunction;
 import wxdgaming.spring.boot.lua.LuaScan;
 import wxdgaming.spring.boot.lua.LuaService;
-import wxdgaming.spring.boot.lua.LuacType;
-
-import java.nio.file.Path;
-import java.util.Arrays;
+import wxdgaming.spring.boot.lua.bean.LuaActor;
 
 /**
  * 测试lua
@@ -27,51 +25,22 @@ import java.util.Arrays;
  */
 @Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {CoreScan.class, DataRedisScan.class, LuaScan.class})
+@ExtendWith(SpringExtension.class)
+@SpringBootApplication
+@SpringBootTest(classes = {CoreScan.class, DataRedisScan.class, LuaScan.class, LuaSpringReflect.class})
 public class LuaTest {
 
-    @Autowired SpringUtil springUtil;
+    @Autowired LuaService luaService;
+    @Autowired LuaSpringReflect luaReflect;
 
-    @Test
-    public void t1() {
-
-        String path = "src/main/lua/";
-        Path script_path = Path.of(path + "/script");
-        FileUtil.walkDirs(script_path, 1).forEach(dir -> {
-            if (dir.equals(script_path)) return;
-            System.out.println(dir + " - " + dir.getFileName());
-        });
-
-        System.out.println("========================================================");
-
-        FileUtil.walkFiles(path + "/util", 99).forEach(file -> {
-            System.out.println(file);
-        });
-
-    }
-
-    protected static LuaService luaService = null;
-
-    static int forCount = 5000;
-
-    public interface T {
-        int t1 = 1;
+    @Before
+    public void before() {
+        luaReflect.content().executorAppStartMethod();
     }
 
     @Test
-    public void t34() {
-        System.out.println(System.currentTimeMillis() + " - " + System.getProperty("user.dir"));
-        // luaBus = LuaBus.buildFromResources(Thread.currentThread().getContextClassLoader(), "script/");
-        luaService = LuaService.of(LuacType.LUA54, true, "src/main/lua");
-        luaService.set("objVar", 1);
-        /*注册函数*/
-        luaService.set("testfun0", new LuaFunction() {
-            @Override public Object doAction(Lua L, Object[] args) {
-                log.info("{}", Arrays.toString(args));
-                return "java 返回值";
-            }
-        });
-        luaService.getRuntime().call("login");
+    public void execute() {
+        luaService.getLuaRuntime().call("onlogin", new LuaActor(9527L, "9527"));
     }
 
 
