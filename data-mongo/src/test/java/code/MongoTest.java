98856bac7e147f95fbc84ab591e1828ff6c90bbd
wxd-gaming <492794628@qq.com>
2025-01-15 11:45:34 +0800

    【新增】mongo测试

--- data-mongo/src/test/java/code/MongoTest.java
+++ data-mongo/src/test/java/code/MongoTest.java
@@ -6,6 +6,7 @@ import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.junit.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -30,13 +31,14 @@ import java.util.List;
 @SpringBootTest(classes = {CoreScan.class, DataMongoScan.class, MongoTest.class})
 public class MongoTest {
 
-    HexId hexId = new HexId(1);
+   static HexId hexId = new HexId(1);
     @Autowired MongoTemplate mongoTemplate;
     @Autowired @Qualifier("secondaryMongoTemplate") MongoTemplate mongoTemplate2;
 
     @Test
+    @RepeatedTest(5)
     public void add() {
-        for (int k = 0; k < 1; k++) {
+        for (int k = 0; k < 10; k++) {
             long nanoTime = System.nanoTime();
             List<TestIndex> list = new ArrayList<>();
             for (int i = 0; i < 2000; i++) {
@@ -54,12 +56,16 @@ public class MongoTest {
     }
 
     @Test
+    @RepeatedTest(5)
     public void count() {
+        System.out.println("hashCode: " + hexId.hashCode());
+        long nanoTime = System.nanoTime();
         MongoCollection<Document> collection = mongoTemplate.createCollection(TestIndex.class);
         ArrayList<Bson> arrayList = new ArrayList<>();
-        arrayList.add(new Document("$match", new Document("username", "24231745071200")));
-        arrayList.add(new Document("$count","count"));
+        arrayList.add(new Document("$match", new Document("username", "91418914685800")));
+        arrayList.add(new Document("$count", "count"));
         AggregateIterable<Document> aggregate = collection.aggregate(arrayList);
+        System.out.println((System.nanoTime() - nanoTime) / 10000 / 100f + " ms");
         for (Document document : aggregate) {
             System.out.println(document);
         }
