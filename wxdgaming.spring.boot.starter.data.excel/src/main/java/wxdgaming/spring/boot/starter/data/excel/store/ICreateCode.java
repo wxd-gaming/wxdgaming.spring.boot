9223009044a874b3555bf8f4cd3b26132b0b1b0f
wxd-gaming <492794628@qq.com>
2025-07-28 14:02:38 +0800

    【重构】新分支，新代码，删除所有历史，重新开始

--- wxdgaming.spring.boot.starter.data.excel/src/main/java/wxdgaming/spring/boot/starter/data/excel/store/ICreateCode.java
+++
@@ -1,9 +0,0 @@
-package wxdgaming.spring.boot.starter.data.excel.store;
-
-import wxdgaming.spring.boot.starter.data.excel.TableData;
-
-public interface ICreateCode {
-
-    void createCode(TableData tableData, String outPath, String packageName, String belong);
-
-}
